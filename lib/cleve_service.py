5d8d7bb81da1239f3087a97be15cb459b004c329
Niklas Mähler <niklas.mahler@regionvasterbotten.se>
2024-04-15 12:10:50 +0200

    test: mock the cleve service inside the test
    This makes use of the unittest.mock package to mock the functions of the
    cleve service. With this, the need for a separate mock class is
    eliminated, and everything is controlled from the test itself.

--- lib/cleve_service.py
+++ lib/cleve_service.py
@@ -1,5 +1,4 @@
 import requests
-import time
 from typing import Any, Dict, Optional
 
 
@@ -9,7 +8,11 @@ class CleveError(Exception):
 
 class Cleve:
 
-    def __init__(self, host: str, port: int, key: Optional[str] = None):
+    def __init__(
+            self,
+            host: str = "localhost",
+            port: int = 8080,
+            key: Optional[str] = None):
         self.uri = f"http://{host}:{port}/api"
         self.key = key
 
@@ -167,47 +170,3 @@ class Cleve:
             )
 
         return r.json()
-
-
-class CleveMock(Cleve):
-
-    def __init__(self, runs: Optional[Dict[str, Dict]] = None):
-        self.key = "supersecretapikey"
-        self.runs = {}
-        if runs is not None:
-            self.runs = runs
-
-    def get_runs(self, platform: Optional[str] = None, state: Optional[str] = None) -> Dict[str, Dict]:
-        runs = {}
-        for run_id, run in self.runs.items():
-            last_state = sorted(
-                run["state_history"], key=lambda x: x["time"], reverse=True)[0]["state"]
-            if platform and run["platform"] != platform:
-                continue
-            if state and last_state != state:
-                continue
-            runs[run_id] = run
-        return runs
-
-    def add_run(self, key: str, run: Dict):
-        if key != self.key:
-            raise CleveError("invalid API key")
-        if "run_id" not in run:
-            raise CleveError("run must have a run_id")
-        if "state_history" not in run:
-            raise CleveError("run must have a state_history")
-        if "platform" not in run:
-            raise CleveError("run must have a platform")
-        self.runs[run["run_id"]] = run
-
-    def update_run(self, key: str, run_id: str, state: Optional[str] = None, analysis: Optional[Dict] = None):
-        if key != self.key:
-            raise CleveError("invalid API key")
-        if run_id not in self.runs:
-            raise CleveError(f"run {run_id} not found")
-
-        if state is not None:
-            self.runs[run_id]["state_history"].append(
-                {"state": state, "time": time.time()})
-        if analysis is not None:
-            self.runs[run_id]["analysis"].append(analysis)
